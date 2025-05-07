2a5c922b025ea8e6e0b2a812e8da9c835c69360e
Max Fatouros <max.fatouros@gmail.com>
2025-05-07 23:59:39 +0200

    update main.jl and test.jl files

--- main.jl
+++ main.jl
@@ -2,26 +2,52 @@ using BenchmarkTools
 
 include("simulation.jl")
 include("plots.jl")
+include("geometry.jl")
 println()
 
-simulation = Simulation(
-    100,
-)
+function single_sphere_scene()
+    simulation = Simulation2DQ9(
+        1000,
+        # 100_000,
+    )
+    add_sphere!(
+        simulation;
+        position=(50, 50),
+        radius=25,
+    )
+    return animate_speeds!(simulation)
+end
 
+simulation = Simulation2DQ9(
+    1000,
+    # 100_000,
+)
 add_sphere!(
     simulation;
     position=(50, 50),
     radius=25,
 )
+animate_speeds_live!(simulation)
 
-simulations = run!(simulation)
-# animate_speeds_live(
-#     simulation,
+# simulation = Simulation{2}(
+#     # 1000,
 #     100_000,
 # )
 
-# for i in CartesianIndices(simulation.grid[1,1,:,:])
-#     @show i
-# end
+# add_sphere!(
+#     simulation;
+#     position=(40, 60),
+#     radius=10,
+# )
+# add_sphere!(
+#     simulation;
+#     position=(60, 40),
+#     radius=10,
+# )
 
-# @info size(simulation.grid)
+# simulations = run!(simulation)
+# animate_speeds_with_slider(
+#     simulations
+# )
+# #
+# animate_speeds_live(simulation)
--- test.jl
+++ test.jl
@@ -3,13 +3,25 @@ using BenchmarkTools
 
 include("simulation.jl")
 include("plots.jl")
+include("geometry.jl")
 
-simulation = Simulation(
-    100,
+# simulation = Simulation{2, 9}(
+#     100_000,
+# )
+
+# add_sphere!(
+#     simulation;
+#     position=(50, 50),
+#     radius=25,
+# )
+
+simulation = Simulation3DQ15(
+    100;
+    divisions=(100, 50, 50),
 )
 
 add_sphere!(
     simulation;
-    position=(50, 50),
-    radius=25,
+    position=(30, 25, 25),
+    radius=15,
 )
