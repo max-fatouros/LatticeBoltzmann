2b9e08ce1a8a6b1d59affcaaae8eb7457271f33d
Max Fatouros <max.fatouros@gmail.com>
2025-05-12 16:02:16 +0200

    add workflows.jl

---
+++ workflows.jl
@@ -0,0 +1,37 @@
+include("simulation.jl")
+include("plots.jl")
+
+output_dir = "report/media"
+mkpath(output_dir)
+
+function single_sphere_scene()
+    simulation = Simulation2DQ9(
+        1000,
+        # 100_000,
+    )
+    add_sphere!(
+        simulation;
+        position=(50, 50),
+        radius=25,
+    )
+    return animate_speeds!(simulation)
+end
+
+function make_direction_plots()
+    with_theme(
+        Theme(;
+            fontsize=20,
+            markersize=20,
+        ),
+    ) do
+        f = plot_directions(Simulation2DQ9(100; divisions=(10, 10)))
+        display(f)
+        path = joinpath(output_dir, "directions_d2q9.png")
+        save(path, f)
+
+        f = plot_directions(Simulation3DQ15(100; divisions=(10, 10, 10)))
+        display(f)
+        path = joinpath(output_dir, "directions_d3q15.png")
+        return save(path, f)
+    end
+end
