9c8f5b1a6b40e7be1a4cde654dbedb0feadca559
Max Fatouros <max.fatouros@gmail.com>
2025-05-13 22:54:42 +0200

    speed of sound results

--- workflows.jl
+++ workflows.jl
@@ -1,8 +1,24 @@
 include("simulation.jl")
 include("plots.jl")
 
-output_dir = "report/media"
-mkpath(output_dir)
+media_dir = "report/media"
+animations_dir = "animations"
+mkpath(media_dir)
+mkpath(animations_dir)
+
+"""
+wrap Makie.set_theme! so that revise updates this.
+"""
+function set_theme()
+    return set_theme!(
+        Theme(;
+            fontsize=20,
+            linewidth=2,
+            markersize=6,
+        ),
+    )
+end
+set_theme()
 
 function single_sphere_scene()
     simulation = SimulationD2Q9()
@@ -26,27 +42,104 @@ function make_velocity_plots()
     ) do
         f = plot_velocities(SimulationD2Q9((10, 10)))
         display(f)
-        path = joinpath(output_dir, "velocities_d2q9.png")
+        path = joinpath(media_dir, "velocities_d2q9.png")
         save(path, f)
 
         f = plot_velocities(SimulationD3Q15((10, 10, 10)))
         display(f)
-        path = joinpath(output_dir, "velocities_d3q15.png")
+        path = joinpath(media_dir, "velocities_d3q15.png")
         return save(path, f)
     end
+end
+
+"""
+Returns a vector of fit parameters in increasing order.
+i.e. returns (p_1, p_2) for a_1 + a_2*X = Y
+"""
+function fit(X, Y)
+    U = hcat.(1, X)
+    P = inv(U' * U) * U' * Y
+    return P
 end
 
 function make_pulse_2D()
     simulation = SimulationD2Q9()
 
-    add_source!(simulation, (5, :), 1, 0.4)
+    add_source!(simulation, (3, :), 1, 0.4)
     update!(simulation)
     pop!(simulation.sources)
+    return simulation
+end
 
-    for i ∈ 1:100
+function make_pulse_3D()
+    simulation = SimulationD3Q15((600, 25, 25))
+
+    add_source!(simulation, (3, :, :), 1, 0.4)
+    update!(simulation)
+    pop!(simulation.sources)
+    return simulation
+end
+
+function record_pulse_2D()
+    simulation = make_pulse_2D()
+    path = joinpath(animations_dir, "pulse-2d.mp4")
+    return animate!(
+        simulation,
+        plot_speeds;
+        steps=600,
+        filename=path,
+        show_every=100,
+    )
+end
+
+function plot_speed_of_sound_fit(dimensions=2)
+    set_theme()
+    CairoMakie.activate!()
+    simulation = nothing
+    times = nothing
+    dims = nothing
+    path = nothing
+    if 2 == dimensions
+        simulation = make_pulse_2D()
+        dims = 2
+        times = collect(1:500)
+        path = joinpath(media_dir, "speed_fit_2d.png")
+    elseif 3 == dimensions
+        simulation = make_pulse_3D()
+        dims = (2, 3)
+        times = collect(1:500)
+        path = joinpath(media_dir, "speed_fit_3d.png")
+    end
+
+    prog = Progress(length(times))
+    peak_positions = []
+    for i ∈ times
+        next!(prog)
         speeds = get_speeds(simulation)
+        peak_pos = argmax(mean(speeds; dims=dims))[1]
+        push!(peak_positions, peak_pos)
         update!(simulation)
     end
 
-    return simulation
+    fig = Figure()
+    ax = CairoMakie.Axis(fig[1, 1])
+    ax.title = "$(dimensions)D pulse peak-fit over time"
+    ax.xlabel = "time [lt]"
+    ax.ylabel = "space [lx]"
+    CairoMakie.scatter!(ax, peak_positions; label="peak-positions")
+
+    parameters = fit(times, peak_positions)
+    fit_line = parameters[1] .+ parameters[2] * times
+
+    CairoMakie.lines!(ax, fit_line; color=:red, linestyle=:dot, label="fit")
+
+    axislegend(ax; halign=:left)
+
+    display(fig)
+
+    save(path, fig)
+
+    @show fit_diff_std = std(fit_line .- peak_positions)
+    @show "fit_slope: $(parameters[2])"
+    @show parameters[2] - 1 / sqrt(3)
 end
