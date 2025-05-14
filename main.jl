de13bc42c300a37660f7aa698369db1b715e67c2
Max Fatouros <max.fatouros@gmail.com>
2025-05-14 17:29:02 +0200

    remove workflows from main

--- main.jl
+++ main.jl
@@ -6,28 +6,3 @@ includet("plots.jl")
 includet("geometry.jl")
 includet("workflows.jl")
 println()
-
-# make_direction_plots()
-
-# simulation = Simulation{2}(
-#     # 1000,
-#     100_000,
-# )
-
-# add_sphere!(
-#     simulation;
-#     position=(40, 60),
-#     radius=10,
-# )
-# add_sphere!(
-#     simulation;
-#     position=(60, 40),
-#     radius=10,
-# )
-
-# simulations = run!(simulation)
-# animate_speeds_with_slider(
-#     simulations
-# )
-# #
-# animate_speeds_live(simulation)
